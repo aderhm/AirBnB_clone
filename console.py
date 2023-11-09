4a472a5fc2a4f3caeb6c9919f53bd2b0bf3207ad
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-11-09 23:33:52 +0100

    Handle create command

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ This file defines the HBnB console.
 """
 
 import cmd
+from models import base_model, storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +13,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    __classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program.
@@ -28,6 +30,18 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing when receiving an empty line.
         """
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of a given class and prints its id.
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            if arg == "BaseModel":
+                print("{}".format(base_model.BaseModel().id))
+                storage.save()            
 
 
 if __name__ == '__main__':
