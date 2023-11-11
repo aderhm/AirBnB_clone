721fbf294d814d8d06b21271b066431e6a254bdb
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-11-11 21:13:31 +0100

    Test User

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+This file contains user.py unit tests.
+"""
+
+import unittest
+import datetime
+from models.user import User
+from models.base_model import BaseModel
+
+
+class UserCase(unittest.TestCase):
+    """Tests instances and methods from user class.
+    """
+
+    user = User()
+
+    def test_class_existence(self):
+        """Tests if the class exists.
+        """
+        self.assertEqual(str(type(self.user)), "<class 'models.user.User'>")
+
+    def test_user_inheritance(self):
+        """Tests if User is an instance of BaseModel.
+        """
+        self.assertIsInstance(self.user, BaseModel)
+
+    def test_has_attributes(self):
+        """Tests if the attributes exist.
+        """
+        self.assertTrue(hasattr(self.user, 'id'))
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertTrue(hasattr(self.user, 'created_at'))
+        self.assertTrue(hasattr(self.user, 'updated_at'))
+
+    def test_attributes_types(self):
+        """Tests attributes types.
+        """
+        self.assertIsInstance(self.user.id, str)
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+        self.assertIsInstance(self.user.created_at, datetime.datetime)
+        self.assertIsInstance(self.user.updated_at, datetime.datetime)
+
+
+if __name__ == '__main__':
+    unittest.main()
