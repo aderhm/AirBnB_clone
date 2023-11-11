bb2993a74ff1b227210978be5de4928c962c2f94
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-11 15:46:35 +0100

    more_classes

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    module for the class city
+"""
+from models.base_model import BaseModel
+
+
+class city(BaseModel):
+    """
+        the class City
+    """
+    state_id = ""
+    name = ""
